eb6931d6778457daab80e847475dfec9a689806d
c.guirado <cgboluda@gmail.com>
2015-11-23 10:43:28 +0100

    cliente hasta envios

--- client.py
+++ client.py
@@ -9,19 +9,19 @@ import sys
 
 # Cliente UDP simple.
 
+#Comparar argumentos
+if len(sys.argv) != 3:
+    sys.exit("Usage: python client.py method receiver@IP:SIPport")
 # Dirección IP del servidor.
 SERVER = 'localhost'
-PORT = 6001
 Metodo = sys.argv[1]
 Direccion = sys.argv[2]
 
 Separar = Direccion.split(":")
-Login = Separar[0]
-Puerto = Separar[1]
-print(Login)
-print(Puerto)
-# Contenido que vamos a enviar
-LINE = '¡Hola mundo!'
+part1 = Separar[0]
+PORT = int(Separar[1])
+Login = part1.split("@")[0]
+IP = part1.split("@")[1]
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -29,17 +29,22 @@ my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((SERVER, PORT))
 
 #Contenido que enviamos
-if not len(sys.argv) != 2:
-    sys.exit("Usage: python client.py method receiver@IP:SIPport")
+
 if Metodo == "INVITE":
-    USER = Direccion
+    LINE = ("INVITE sip:" + Login + "@" + IP +"SIP/2.0")
+if Metodo == "BYE":
+    LINE = ("BYE sip:" + Login + "@" + IP + "SIP/2.0")
 print("Enviando: " + LINE)
 my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
 data = my_socket.recv(1024)
 
 print('Recibido -- ', data.decode('utf-8'))
+Recibido = data.decode('utf-8')
+if Recibido == "100 TRYING" and "180 RING" and "200 OK":
+    LINE = ("ACK sip:" + Login + "@" + IP + "SIP/2.0")
+    my_socket.send(bytes(LINE, 'utf-8') + b'\r\n')
 print("Terminando socket...")
 
-# Cerramos todo
+# Cerramos todo 
 my_socket.close()
 print("Fin.")
